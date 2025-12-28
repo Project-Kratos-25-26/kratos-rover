56930d7d25cc8f242d3e4fd478e2713488dabe47
G33k8 <atharva08106@gmail.com>
2025-12-28 08:41:27 +0530

    nav2

---
+++ kratos_nav2/launch/kratos_nav2.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
+
+
+def generate_launch_description():
+
+    nav2_launch = PathJoinSubstitution([
+        FindPackageShare('nav2_bringup'),
+        'launch',
+        'navigation_launch.py'
+    ])
+
+    params_file = PathJoinSubstitution([
+        FindPackageShare('kratos_nav2'),
+        'config',
+        'nav2_params.yaml'
+    ])
+
+    return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(nav2_launch),
+            launch_arguments={
+                'use_sim_time': 'true',
+                'autostart': 'true',
+                'slam': 'false',
+                'params_file': params_file
+            }.items()
+        )
+    ])
+
